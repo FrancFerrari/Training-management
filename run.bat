46ae94c12cc013232d980eb20e01dffc85d16f55
FrancFerrari <francesco.ferrari98@gmail.com>
2026-03-09 18:20:11 +0100

    fix jar creation

--- run.bat
+++ run.bat
@@ -4,14 +4,21 @@ REM Script to run Training Manager on Windows
 setlocal enabledelayedexpansion
 
 set SCRIPT_DIR=%~dp0
-set JAR_FILE=%SCRIPT_DIR%target\allenamento-manager-1.0.0-jar-with-dependencies.jar
+set JAR_FILE=%SCRIPT_DIR%allenamento-manager-1.0.0-windows.jar
+set FALLBACK_JAR=%SCRIPT_DIR%target\allenamento-manager-1.0.0-jar-with-dependencies.jar
+
+if not exist "%JAR_FILE%" (
+    set JAR_FILE=%FALLBACK_JAR%
+)
 
 echo Starting Training Manager...
 echo JAR: %JAR_FILE%
 
 if not exist "%JAR_FILE%" (
     echo Error: JAR file not found!
-    echo Run first: mvn clean package assembly:single
+    echo Run first: mvn clean package -Pwindows
+    echo Optional copy in project root:
+    echo copy target\allenamento-manager-1.0.0-jar-with-dependencies.jar allenamento-manager-1.0.0-windows.jar
     pause
     exit /b 1
 )
